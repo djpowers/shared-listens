053db02a7780c6d82221b5b409c7f08dafce0642
Dave Powers <djpowers89@gmail.com>
2017-11-15 00:20:09 -0500

    Add error/loading handling for shared music, render lists of recent tracks

--- src/SharedMusic.js
+++ src/SharedMusic.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import TrackList from './TrackList';
 
 const API_KEY = '';
@@ -8,6 +8,8 @@ class SharedMusic extends React.Component {
   constructor() {
     super();
     this.state = {
+      error: null,
+      isLoaded: false,
       recentTracks: {},
     };
   }
@@ -19,19 +21,32 @@ class SharedMusic extends React.Component {
       .map((key => fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${key}&api_key=${API_KEY}&format=json`)
         .then(res => res.json())
         .then(data => recentTracks[key] = data)
+        .catch(error =>
+          this.setState({
+            isLoaded: true,
+            error,
+          }),
+        )
         .then(() => this.setState({
+          isLoaded: true,
           recentTracks,
         }))
       ));
   }
 
   render() {
+    const { error, isLoaded, recentTracks } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading ...</div>;
+    }
     return (
       <div>
         {
           Object
             .keys(this.props.users)
-            .map(key => <TrackList key={key} index={key} recentTracks={this.state.recentTracks[key]} />)
+            .map(key => <TrackList key={key} index={key} recentTracks={recentTracks[key]} />)
         }
       </div>
     );
@@ -39,3 +54,9 @@ class SharedMusic extends React.Component {
 }
 
 export default SharedMusic;
+
+SharedMusic.propTypes = {
+  users: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+};
--- src/TrackList.js
+++ src/TrackList.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class TrackList extends React.Component {
   render() {
     const { index, recentTracks } = this.props;
+    if (!recentTracks) return null;
+    const tracksList = recentTracks.recenttracks.track.map(track => <li>{track.name}</li>);
     return (
       <div>
         <h2 className="user-name">{index}</h2>
+        <ul className="track-lists">{tracksList}</ul>
       </div>
     );
   }
 }
 
 export default TrackList;
+
+TrackList.propTypes = {
+  index: PropTypes.string.isRequired,
+  recentTracks: PropTypes.shape({
+    recenttracks: PropTypes.shape({
+      track: PropTypes.array.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
